c49a13d9bfaea049862df098d6f8985dc8c28bf3
Shaik Abdul Vahid <59871200+SAW-NAAZ@users.noreply.github.com>
2024-09-17 10:00:09 +0530

    Update Greeter.java
    Hello I made the changes

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Hello Team Good afternoon Welcome back !!!");
+	  System.out.println("Hello from Main Method & Hello Team Good Morning Welcome back !!!");
   }
 	
 }
